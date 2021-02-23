4231c4c20e6af3490822923ccc6cd30c559457d0
Cole Johnson <cburnjohnson@gmail.com>
2021-02-22 20:24:50 -0800

    initial call for data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
+import React, {useEffect} from 'react'
+import axios from 'axios'
+
 function App() {
+
+  useEffect(() => {
+    async function getData() {
+      const results = await axios.get('https://api.covidtracking.com/v1/us/daily.json')
+      console.log(results);
+    }
+    getData()
+  })
+
   return (
     <div>COVID MAP</div>
   );
