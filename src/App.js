5d3ecb69307b6a70eaf8dfa49e8b76418832902b
Cole Johnson <cburnjohnson@gmail.com>
2021-02-24 20:39:15 -0800

    added initial US map and stats

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 
-function App() {
+import USMap from './components/maps/USMap'
 
+function App() {
+  const [currentUSStats, setCurrentUSStates] = useState({})
   useEffect(() => {
     async function getData() {
-      const results = await axios.get('https://api.covidtracking.com/v1/us/daily.json')
-      console.log(results);
+      const results = await axios.get('https://api.covidtracking.com/v1/us/current.json')
+      setCurrentUSStates(results.data[0])
     }
     getData()
   })
 
   return (
-    <div>COVID MAP</div>
+    <div>
+      <h1>COVID MAP</h1>
+      <div className="stats-container">
+        <div className="total-stats">
+          <h2>TOTAL</h2>
+          <div>TESTED POSITIVE: {currentUSStats.positive}</div>
+          <div>TESTED NEGATIVE: {currentUSStats.negative}</div>
+          <div>HOSPITALIZED: {currentUSStats.hospitalizedCumulative}</div>
+          <div>ICU: {currentUSStats.inIcuCumulative}</div>
+          <div>DEATHS: {currentUSStats.death}</div>
+        </div>
+        
+        <div className="current-stats">
+          <h2>CURRENTLY</h2>
+          <div>HOSPITALIZED: {currentUSStats.hospitalizedCurrently}</div>
+          <div>ICU: {currentUSStats.inIcuCurrently}</div>
+        </div>
+      </div>
+     
+      <USMap />
+    </div>
+    
   );
 }
 
