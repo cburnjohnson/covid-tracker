cf42cba14980de13af346678f28139b7773102c1
Cole Johnson <cburnjohnson@gmail.com>
2021-02-26 14:37:08 -0800

    changed map

--- src/components/maps/USMap.js
+++ src/components/maps/USMap.js
@@ -1,55 +1,76 @@
-import React, { useState, useEffect } from "react";
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
-import { scaleQuantize } from "d3-scale";
-import { csv } from "d3-fetch";
+import React from "react";
+import { geoCentroid } from "d3-geo";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Marker,
+  Annotation
+} from "react-simple-maps";
 
-const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/counties-10m.json";
+import allStates from "../../data/allstates.json";
 
-const colorScale = scaleQuantize()
-  .domain([1, 10])
-  .range([
-    "#ffedea",
-    "#ffcec5",
-    "#ffad9f",
-    "#ff8a75",
-    "#ff5533",
-    "#e2492d",
-    "#be3d26",
-    "#9a311f",
-    "#782618"
-  ]);
+const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json";
 
-const USMap = () => {
-  const [data, setData] = useState([]);
+const offsets = {
+  VT: [50, -8],
+  NH: [34, 2],
+  MA: [30, -1],
+  RI: [28, 2],
+  CT: [35, 10],
+  NJ: [34, 1],
+  DE: [33, 0],
+  MD: [47, 10],
+  DC: [49, 21]
+};
 
-  useEffect(() => {
-    // https://www.bls.gov/lau/
-    csv("/US-stats.csv").then(counties => {
-      console.log(counties)
-      setData(counties);
-    });
-  }, []);
-
+const MapChart = () => {
   return (
-    <>
-      <ComposableMap projection="geoAlbersUsa">
-        <Geographies geography={geoUrl}>
-          {({ geographies }) =>
-            geographies.map(geo => {
-              const cur = data.find(s => s.id === geo.id);
+    <ComposableMap projection="geoAlbersUsa">
+      <Geographies geography={geoUrl}>
+        {({ geographies }) => (
+          <>
+            {geographies.map(geo => (
+              <Geography
+                key={geo.rsmKey}
+                stroke="#FFF"
+                geography={geo}
+                fill="#DDD"
+              />
+            ))}
+            {geographies.map(geo => {
+              const centroid = geoCentroid(geo);
+              const cur = allStates.find(s => s.val === geo.id);
               return (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  fill={colorScale(cur ? cur.unemployment_rate : "#EEE")}
-                />
+                <g key={geo.rsmKey + "-name"}>
+                  {cur &&
+                    centroid[0] > -160 &&
+                    centroid[0] < -67 &&
+                    (Object.keys(offsets).indexOf(cur.id) === -1 ? (
+                      <Marker coordinates={centroid}>
+                        <text y="2" fontSize={14} textAnchor="middle">
+                          {cur.id}
+                        </text>
+                      </Marker>
+                    ) : (
+                      <Annotation
+                        subject={centroid}
+                        dx={offsets[cur.id][0]}
+                        dy={offsets[cur.id][1]}
+                      >
+                        <text x={4} fontSize={14} alignmentBaseline="middle">
+                          {cur.id}
+                        </text>
+                      </Annotation>
+                    ))}
+                </g>
               );
-            })
-          }
-        </Geographies>
-      </ComposableMap>
-    </>
+            })}
+          </>
+        )}
+      </Geographies>
+    </ComposableMap>
   );
 };
 
-export default USMap;
+export default MapChart;
